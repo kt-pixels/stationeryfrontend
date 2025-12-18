294432135e97b8530fafc4cce0ae55464c67d490
kirtan kaushik <116414527+kt-pixels@users.noreply.github.com>
2025-12-18 19:50:00 +0530

    Update api file

--- src/services/api.js
+++ src/services/api.js
@@ -4,7 +4,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://stationerybackend.onrender.com/api",
 });
 
 api.interceptors.request.use((config) => {
